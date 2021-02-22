90d64276055fadcf464e7b7810a48e8a4870a23b
Jefferson Nikson <jefferson.nikson@gmail.com>
2021-02-22 12:26:39 -0300

    Auto stash before rebase of "Projeto-Edgle-Churrascaria/master"

--- src/main/java/br/com/churrascaria/beans/EnderecoPaginas.java
+++ src/main/java/br/com/churrascaria/beans/EnderecoPaginas.java
@@ -1,16 +1,27 @@
 package br.com.churrascaria.beans;
 
-
 public final class EnderecoPaginas {
+<<<<<<< Updated upstream
 	
+=======
+>>>>>>> Stashed changes
 
 	public EnderecoPaginas() {
 		throw new UnsupportedOperationException("Esta classe não deve ser instanciada!");
 	}
-	
+
 	private static final String REDIRECT_TRUE = "?faces-redirect=true";
-	
-	public static final String PAGINA_PRINCIPAL_FUNCIONARIO = "/paginas/protegidas/pessoas/funcionario/index.xhtml" + REDIRECT_TRUE;
+
+	public static final String PAGINA_PRINCIPAL_FUNCIONARIO = "/paginas/protegidas/pessoas/funcionario/index.xhtml"
+			+ REDIRECT_TRUE;
+
+	public static final String PAGINA_PRINCIPAL_MESA = "/paginas/protegidas/gestao/mesas/index.xhtml" + REDIRECT_TRUE;
+
+	public static final String PAGINA_PRINCIPAL_CATEGORIAPRODUTO = "/paginas/protegidas/catalogo/produtos/index.xhtml"
+			+ REDIRECT_TRUE;
+
+	public static final String PAGINA_PRINCIPAL_ENTREGADOR = "/paginas/protegidas/pessoas/entregadores/index.xhtml"
+			+ REDIRECT_TRUE;
 
 	public static final String PAGINA_PRINCIPAL_OBSERVACAO ="/paginas/protegidas/catalogo/observacoes/index.xhtml"+ REDIRECT_TRUE;
 	
